cb9c0e98f5af3a8d509a3ca6dd379ecabb6ae7df
FroppyDev <potatokun100@gmail.com>
2025-12-21 15:11:50 -0600

    Api para web

--- app/main.py
+++ app/main.py
@@ -40,12 +40,12 @@ app.add_middleware(
         "https://memotoriapi.onrender.com",
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # -------------------- SEED --------------------
 
