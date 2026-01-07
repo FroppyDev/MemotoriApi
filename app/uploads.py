00b26f571a1726135df02277f2736e540dfe6c41
FroppyDev <potatokun100@gmail.com>
2026-01-06 20:49:04 -0600

    lo mismo

--- app/uploads.py
+++ app/uploads.py
@@ -1,4 +1,4 @@
-import os
+import uuid
 import shutil
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from pathlib import Path
@@ -15,14 +15,13 @@ async def upload_image(file: UploadFile = File(...)):
     if file.content_type not in ["image/png", "image/jpeg", "image/jpg"]:
         raise HTTPException(status_code=400, detail="Formato no soportado")
 
-    filename = file.filename.replace(" ", "_")
+    ext = Path(file.filename).suffix
+    filename = f"{uuid.uuid4()}{ext}"
     filepath = IMAGES_DIR / filename
 
     with open(filepath, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    file_url = f"https://memotoriapi.onrender.com/imagenes/tarjetas/{filename}"
-
     return {
-        "url": file_url
+        "url": f"https://memotoriapi.onrender.com/imagenes/tarjetas/{filename}"
     }
